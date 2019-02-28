830657b778e673e5a9e49d23c1e8d17da2a95d12
HuLi <1552980358@qq.com>
2019-02-28 21:16:08 +0800

    Fix icon size, fix exception during disable all

--- app/src/main/java/com/earth/OsToolkit/ShortcutDisableActivity.kt
+++ app/src/main/java/com/earth/OsToolkit/ShortcutDisableActivity.kt
@@ -3,6 +3,7 @@ package com.earth.OsToolkit
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 
 import com.earth.OsToolkit.base.BaseKotlinOperation.Companion.ShortToast
 
@@ -26,11 +27,16 @@ class ShortcutDisableActivity: Activity() {
                 .getStringSet("added", setOf<String>())
             for (i in savedList!!) {
                 Shell.su("pm disable $i").exec()
+                Log.i("SDA", i)
             }
             runOnUiThread {
                 ShortToast(this, "Finish")
-                finish()
             }
         }.start()
+    }
+
+    override fun onResume() {
+        finish()
+        super.onResume()
     }
 }
