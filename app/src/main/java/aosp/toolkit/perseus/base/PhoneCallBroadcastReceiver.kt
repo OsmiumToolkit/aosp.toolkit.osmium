d200615fd400ccfe44ccf23de7e2d07a824a3ff0
HuLi <1552980358@qq.com>
2019-05-11 13:15:53 +0800

    Close phone call launch

--- app/src/main/java/aosp/toolkit/perseus/base/PhoneCallBroadcastReceiver.kt
+++ app/src/main/java/aosp/toolkit/perseus/base/PhoneCallBroadcastReceiver.kt
@@ -3,6 +3,7 @@ package aosp.toolkit.perseus.base
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import aosp.toolkit.perseus.SplashActivity
 import aosp.toolkit.perseus.base.BaseOperation.Companion.ShortToast
 
@@ -20,13 +21,7 @@ class PhoneCallBroadcastReceiver {
     class OnStartAppBroadcastReceiver: BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             try {
-                intent?.let {
-                    if (it.data?.schemeSpecificPart?.substring(2) == "0813") {
-                        context!!.startActivity(Intent(context, SplashActivity::class.java))
-                    }
-                    return
-                }
-                return
+                context!!.startActivity(Intent(Intent.ACTION_MAIN).setClass(context, SplashActivity::class.java).setFlags(FLAG_ACTIVITY_NEW_TASK))
             } catch (e: Exception) {
                 ShortToast(context!!, e)
             }
